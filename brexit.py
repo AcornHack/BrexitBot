587bd9ff843b92aa76973a9ac576a4995012b3bc
sean22499 <sean.irg22499@gmail.com>
2019-01-27 13:19:43 +0000

    Work in Progress

---
+++ brexit.py
@@ -0,0 +1,62 @@
+leave = False
+while not leave:
+    print("""
+
+
+
+I am a BrexitBot,
+0.What is Brexit?
+1.What is a Referendum?
+2.Who can vote?
+3.Who cant vote?
+4.Why is voting important?
+5.Why do we vote?
+6.Who make the elections 
+7.Who is the electoral commision?
+8.What are the main parties?
+9.What is the voting system?
+10.Who is the house of commons?
+11.Who is an MP?
+12.Who is the PM?
+13.What is the Transition Period?
+14.When will we split?
+
+
+
+""")
+    num = input("What would you like to find out about?    ")
+    if num == "1":
+        print("A general vote by the electorate on a single political question which has been referred to them for a direct decision.")
+    if num == "2":
+        print("""To vote at the UK general election you must be registered to vote and:
+-18 years of age or over on polling day
+-be a British, Irish or qualifying Commonwealth citizen
+-be resident at an address in the UK (or a UK citizen living abroad who has been registered -to vote in the UK in the last 15 years)
+-not be legally excluded from voting""")
+    if num == "0":
+        print("Brexit is Britain's exit from the European Union.That means leaving its single market and customs union, cutting legal ties, ending the automatic right for EU citizens to move to Britain, and reducing or ending budget payments to Brussels.")
+    if num == "3":
+        print("")
+    if num == "4":
+        print("")
+    if num == "5":
+        print("")
+    if num == "6":
+        print("")
+    if num == "7":
+        print("")
+    if num == "8":
+        print("")
+    if num == "9":
+        print("")
+    if num == "10":
+        print("")
+    if num == "11":
+        print("")
+    if num == "12":
+        print("")
+    if num == "13":
+        print("")
+        
+
+    
